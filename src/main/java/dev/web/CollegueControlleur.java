fd09ed48a5c21c750ea947876c40485b689f5ece
julie <julie.bouaye@hotmail.fr>
2019-05-02 17:09:39 +0200

    Ajout de l'annotation Cross

--- src/main/java/dev/web/CollegueControlleur.java
+++ src/main/java/dev/web/CollegueControlleur.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import dev.service.CollegueService;
 @RestController
 
 @RequestMapping("/collegue")
+@CrossOrigin
 public class CollegueControlleur {
 	
 	@Autowired
