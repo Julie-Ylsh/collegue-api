f632d0c9cb4dce6c9a0d2f53d8329ed7d356c3b9
julie <julie.bouaye@hotmail.fr>
2019-05-07 10:07:27 +0200

    Ajout 2 fonctions pour trouver toutes le sphotos et trouver 1 photo en fonction du matricule

--- src/main/java/dev/web/PhotoControlleur.java
+++ src/main/java/dev/web/PhotoControlleur.java
@@ -1,12 +1,17 @@
 package dev.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.entites.Collegue;
@@ -18,21 +23,39 @@ import dev.service.CollegueService;
 @RestController
 @CrossOrigin
 
-@RequestMapping("/collegue/modifphoto")
-public class ModifierPhotoControlleur {
+@RequestMapping("/collegue/photo")
+public class PhotoControlleur {
 	@Autowired
 	private CollegueService collegueService;
 
-	@PatchMapping(path="/{matricule}")
-	public ResponseEntity<String> afficherModifierPhoto(@PathVariable String matricule, @RequestBody PhotoUrl photo) throws CollegueNonTrouveException {
+	@PatchMapping(path = "/{matricule}")
+	public ResponseEntity<String> afficherModifierPhoto(@PathVariable String matricule, @RequestBody PhotoUrl photo)
+			throws CollegueNonTrouveException {
 		try {
 			Collegue collegueAModifier = collegueService.rechercherParMatricule(matricule);
 			collegueService.modifierPhotoUrl(matricule, photo.getPhotoUrl());
-			return ResponseEntity.status(200).body("URL de la photo modifié pour " + collegueAModifier.getNom() + " " + collegueAModifier.getPrenoms() + ". Nouvel Url: " + collegueAModifier.getPhotoUrl());
+			return ResponseEntity.status(200).body("URL de la photo modifié pour " + collegueAModifier.getNom() + " "
+					+ collegueAModifier.getPrenoms() + ". Nouvel Url: " + collegueAModifier.getPhotoUrl());
 		}
-		
+
 		catch (CollegueInvalideException e) {
 			return ResponseEntity.status(404).body("Vous n'avez pas entré les bons paramètres");
 		}
 	}
+
+	@GetMapping(path = "/found")
+	@ResponseBody
+	public Collegue afficherPhoto(@RequestParam("matricule") String matricule) throws CollegueNonTrouveException {
+		return collegueService.rechercherParMatricule(matricule);
+
+	}
+
+	@GetMapping
+	@ResponseBody
+	public List<Collegue> afficherToutesPhoto() {
+
+		return collegueService.list();
+
+	}
+
 }
